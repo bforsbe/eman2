21b42d42eec2e2c0d1950f6c962ab0218ec50ab8
shadow_walker <shadowwalkersb@gmail.com>
2017-02-26 19:57:46 -0600

    debug linux rpath

--- ci_support/run_circle_post.sh
+++ ci_support/run_circle_post.sh
@@ -5,6 +5,13 @@ export PATH="$HOME/miniconda2/bin/:$HOME/EMAN2/bin:$PATH"
 export PYTHONPATH="$HOME/EMAN2/lib:$HOME/EMAN2/bin:$PYTHONPATH"
 export EMAN2DIR=$HOME/EMAN2
 
+sudo apt-get install patchelf
+
+ldd /home/ubuntu/EMAN2/lib/libpyAligner2.so
+ldd /home/ubuntu/EMAN2/lib/libEM2.so
+patchelf --print-rpath /home/ubuntu/EMAN2/lib/libpyAligner2.so
+patchelf --print-rpath /home/ubuntu/EMAN2/lib/libEM2.so
+
 e2version.py
 e2speedtest.py
 e2display.py -h
